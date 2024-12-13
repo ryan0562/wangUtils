9433ed8370d30c0f4b8c81b015fac3b3a9c1cd6f
wangwei <519254178@qq.com>
2024-12-13 15:56:43 +0800

    添加doc

--- playground/src/App.vue
+++ playground/src/App.vue
@@ -1,12 +1,24 @@
 <template>
   <div>
-
+    {{ data }}
   </div>
 </template>
 
 <script>
+import wakerUtils, {_data, WangStorage} from "waker-utils";
+
+
 export default {
   name: 'App',
+  data () {
+    return {
+      data:'',
+    }
+  },
+  created () {
+    debugger;
+    this.data = _data._type.getType({});
+  }
 }
 </script>
 
