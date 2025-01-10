17e058c35ac4786ad43634c3c1c52f5b638cedad
wangwei <519254178@qq.com>
2025-01-10 18:29:00 +0800

    superAxios开发

--- src/App.vue
+++ src/App.vue
@@ -18,31 +18,31 @@ export default {
     }
   },
   created () {
-    // 无特殊需求的只需使用这个一个对象即可 公共 header 可在此配置, 如需多个实例 可按照此方式创建多个进行导出
+
     const Axios = new superAxios(
       {
         headers: {
           Authorization: 111,
         },
-        // businessResolve (data) {
-        //   return data.code > 200
-        // },
-        businessRejectEvent: ({data, error}) => {
+        // resolve业务条件
+        resolveConditions (data) {
+          return data.code < 200
+        },
+        // reject前置钩子
+        beforeRejectHooks: ({data, error}) => {
           if (data) this.$message.error(data.message)
         }
       }
     );
-    Axios.get('/mti-service-platform/getSystemTime').then(res => {
+    Axios.get('/mti-service-platform/getSystemTime',{
+      // beforeRejectHooks:null,
+      // resolveConditions:null,
+    }).then(res => {
       this.$message.success(res.message);
     }).catch(({data, error}) => {
-      debugger;
+      console.error(error)
     })
 
-    Axios.get('/mti-service-platform/aaa').then(res => {
-      debugger;
-    }).catch(({data, error}) => {
-      debugger;
-    })
   }
 }
 </script>
